1401212e52e4496a107a6a497e02d0e52f9fcf68
Sophie Chou <sophie88@cinpos.com>
2025-09-17 10:14:19 +0800

    新增靜態資源物件 『sources』

---
+++ src/app/sources.ts
@@ -0,0 +1,104 @@
+export const sources = {
+  logo: {
+    logoHorizonDark: 'src/assets/images/logo/logo-horizon-dark-mode.png',
+    logoHorizonLight: 'src/assets/images/logo/logo-horizon-light-mode.png',
+    logoSquareDark: 'src/assets/images/logo/logo-square-dark-mode.png',
+    logoSquareLight: 'src/assets/images/logo/logo-square-light-mode.png',
+    logoSmallDark: 'src/assets/images/logo/logo-small-dark-mode.png',
+    logoSmallLight: 'src/assets/images/logo/logo-small-light-mode.png',
+  },
+  character: {
+    egg: {
+      eggBad: 'src/assets/images/character/egg-bad.png',
+      eggNormal: 'src/assets/images/character/egg-normal.png',
+      eggSpecial: 'src/assets/images/character/egg-special.png',
+      eggSuperSpecial: 'src/assets/images/character/egg-super-special.png',
+    },
+    child: {
+      child: 'src/assets/images/character/character-normal.png',
+    },
+    evolution: {
+      mud: 'src/assets/images/character/evolution-mud.png',
+      poinson: 'src/assets/images/character/evolution-poinson.png',
+      burned: 'src/assets/images/character/evolution-burned.png',
+      limbs: 'src/assets/images/character/evolution-limbs.png',
+      strong: 'src/assets/images/character/evolution-strong.png',
+      smooth: 'src/assets/images/character/evolution-smooth.png',
+      carrot: 'src/assets/images/character/evolution-carrot.png',
+      rainbow: 'src/assets/images/character/evolution-rainbow.png',
+      glass: 'src/assets/images/character/evolution-glass.png',
+      cute: 'src/assets/images/character/evolution-cute.png',
+      monster: 'src/assets/images/character/evolution-monster.png',
+      cat: 'src/assets/images/character/evolution-cat.png',
+      fox: 'src/assets/images/character/evolution-fox.png',
+      goose: 'src/assets/images/character/evolution-goose.png',
+    },
+    cooked: {
+      mud: 'src/assets/images/character/cooked-mud.png',
+      poison: 'src/assets/images/character/cooked-poison.png',
+      burned: 'src/assets/images/character/cooked-burned.png',
+      limbs: 'src/assets/images/character/cooked-limbs.png',
+      strong: 'src/assets/images/character/cooked-strong.png',
+      smooth: 'src/assets/images/character/cooked-smooth.png',
+      carrot: 'src/assets/images/character/cooked-carrot.png',
+      rainbow: 'src/assets/images/character/cooked-rainbow.png',
+      glass: 'src/assets/images/character/cooked-glass.png',
+      cute: 'src/assets/images/character/cooked-cute.png',
+      monster: 'src/assets/images/character/cooked-monster.png',
+      cat: 'src/assets/images/character/cooked-cat.png',
+      fox: 'src/assets/images/character/cooked-fox.png',
+      goose: 'src/assets/images/character/cooked-goose.png',
+    },
+    dead: {
+      dead: 'src/assets/images/character/dead.png',
+    },
+    others: {
+      isFreezing: 'src/assets/images/character/is-freezing.png',
+    }
+  },
+  store: {
+    food: {
+      worm: 'src/assets/images/store/worm.png',
+      rice: 'src/assets/images/store/rice.png',
+      chicken: 'src/assets/images/store/chicken.png',
+      candy: 'src/assets/images/store/candy.png',
+      fish: 'src/assets/images/store/fish.png',
+      dogFood: 'src/assets/images/store/dog-food.png',
+      icecream: 'src/assets/images/store/icecream.png',
+    },
+    health: {
+      pill: 'src/assets/images/store/pill.png',
+      ginseng: 'src/assets/images/store/ginseng.png',
+      chineseMedicine: 'src/assets/images/store/chinese-medicine.png',
+      pillForHeadache: 'src/assets/images/store/pill-for-headache.png',
+      pillForDiarrhea: 'src/assets/images/store/pill-for-diarrhea.png',
+      pillForGastricUlcer: 'src/assets/images/store/pill-for-gastric-ulcer.png',
+      pillForFlu: 'src/assets/images/store/pill-for-flu.png',
+      rebornPill: 'src/assets/images/store/reborn-pill.png',
+      freezePill: 'src/assets/images/store/freeze-pill.png',
+    },
+    gifts: {
+      doll: 'src/assets/images/store/doll.png',
+      cellphone: 'src/assets/images/store/cellphone.png',
+    },
+    decorations: {
+      star: 'src/assets/images/store/star.png',
+    }
+  },
+  scene: {
+    roomDayLightOn: 'src/assets/images/scene/room-day-light-on.jpg',
+    roomDayLightOff: 'src/assets/images/scene/room-day-light-off.jpg',
+    roomNightLightOn: 'src/assets/images/scene/room-night-light-on.jpg',
+    roomNightLightOff: 'src/assets/images/scene/room-night-light-off.jpg',
+  },
+  otherIcons: {
+    coin: 'src/assets/images/other-icons/coin.png',
+    store: 'src/assets/images/other-icons/store.png',
+    lamp: 'src/assets/images/other-icons/lamp.png',
+    dirty01: 'src/assets/images/other-icons/dirty01.png',
+    dirty02: 'src/assets/images/other-icons/dirty02.png',
+    dirty03: 'src/assets/images/other-icons/dirty03.png',
+    collection: 'src/assets/images/other-icons/collection.png',
+    feed: 'src/assets/images/other-icons/feed.png',
+  }
+};
